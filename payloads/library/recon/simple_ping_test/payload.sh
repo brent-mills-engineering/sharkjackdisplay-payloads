f816fa2bd2eb668194fe6f0712328b1952b6357d
Darren Kitchen <darren@hak5.org>
2025-03-25 00:33:28 -0500

    Create payload.sh

---
+++ payloads/library/recon/simple_ping_test/payload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Title: Simple Ping Test
+# Author: Hak5Darren
+# Props: Korben
+# Description: Pings $IP and shows basic results
+LED SETUP
+IP="8.8.8.8"
+source /usr/lib/spinner_tools
+CREATE_SPINNER "Starting DHCP"
+  NETMODE DHCP_CLIENT
+STOP_SPINNER
+WAIT_FOR_LINK
+WAIT_FOR_IP
+SCREEN_WRITE "Pinging IP:"
+SCREEN_WRITE "$IP"
+ping $IP -c 2 && TEST="success" || TEST="fail"
+if [ "$TEST" = "success" ]; then LED FINISH; SCREEN_WRITE "Success"; fi
+if [ "$TEST" = "fail" ]; then LED FAIL; SCREEN_WRITE "Failed"; fi
